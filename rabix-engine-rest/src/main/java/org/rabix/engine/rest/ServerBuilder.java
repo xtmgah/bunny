303fc90f6b4e080a6b21ba776f2fefc969ecb2c3
Janko Simonovic <janko.simonovic@sbgenomics.com>
2016-03-23 15:25:18 +0100

    fix guice module

--- rabix-engine-rest/src/main/java/org/rabix/engine/rest/ServerBuilder.java
+++ rabix-engine-rest/src/main/java/org/rabix/engine/rest/ServerBuilder.java
@@ -19,11 +19,15 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.rabix.common.config.ConfigModule;
 import org.rabix.engine.EngineModule;
+import org.rabix.engine.rest.api.ExecutableHTTPService;
 import org.rabix.engine.rest.api.TaskHTTPService;
+import org.rabix.engine.rest.api.impl.ExecutableHTTPServiceImpl;
 import org.rabix.engine.rest.api.impl.TaskHTTPServiceImpl;
 import org.rabix.engine.rest.db.TaskDB;
 import org.rabix.engine.rest.plugin.BackendPluginConfig;
 import org.rabix.engine.rest.plugin.BackendPluginRegister;
+import org.rabix.engine.rest.service.ExecutableService;
+import org.rabix.engine.rest.service.TaskService;
 import org.rabix.engine.rest.service.impl.ExecutableServiceImpl;
 import org.rabix.engine.rest.service.impl.TaskServiceImpl;
 
@@ -56,11 +60,11 @@ public class ServerBuilder {
           @Override
           protected void configure() {
             bind(TaskDB.class).in(Scopes.SINGLETON);
-            bind(ExecutableServiceImpl.class).in(Scopes.SINGLETON);
-            bind(TaskServiceImpl.class).in(Scopes.SINGLETON);
-            bind(TaskServiceImpl.class).in(Scopes.SINGLETON);
+            bind(TaskService.class).to(TaskServiceImpl.class).in(Scopes.SINGLETON);
+            bind(ExecutableService.class).to(ExecutableServiceImpl.class).in(Scopes.SINGLETON);
             bind(BackendPluginRegister.class).in(Scopes.SINGLETON);
             bind(BackendPluginConfig.class).in(Scopes.SINGLETON);
+            bind(ExecutableHTTPService.class).to(ExecutableHTTPServiceImpl.class);
             bind(TaskHTTPService.class).to(TaskHTTPServiceImpl.class).in(Scopes.SINGLETON);
           }
         }));
