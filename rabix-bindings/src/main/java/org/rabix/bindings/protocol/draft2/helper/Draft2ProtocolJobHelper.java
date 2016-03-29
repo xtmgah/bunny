6b0317519b10fe0af08328cba482834b72df61b9
Janko Simonovic <janko.simonovic@sbgenomics.com>
2016-03-29 17:11:53 +0200

    fix app resolving

--- rabix-bindings/src/main/java/org/rabix/bindings/protocol/draft2/helper/Draft2ProtocolJobHelper.java
+++ rabix-bindings/src/main/java/org/rabix/bindings/protocol/draft2/helper/Draft2ProtocolJobHelper.java
@@ -1,11 +1,9 @@
 package org.rabix.bindings.protocol.draft2.helper;
 
-import java.io.IOException;
-
 import org.rabix.bindings.BindingException;
 import org.rabix.bindings.ProtocolJobHelper;
-import org.rabix.bindings.helper.URIHelper;
 import org.rabix.bindings.model.Job;
+import org.rabix.bindings.protocol.draft2.Draft2DocumentReferenceResolver;
 import org.rabix.bindings.protocol.draft2.Draft2JobProcessor;
 import org.rabix.bindings.protocol.draft2.bean.Draft2Job;
 import org.rabix.bindings.protocol.draft2.bean.Draft2JobApp;
@@ -15,15 +13,16 @@ import org.rabix.common.json.BeanSerializer;
 
 public class Draft2ProtocolJobHelper implements ProtocolJobHelper {
 
+  private final Draft2DocumentReferenceResolver documentResolver;
+
+  public Draft2ProtocolJobHelper() {
+    this.documentResolver = new Draft2DocumentReferenceResolver();
+  }
+  
   public Draft2Job getJob(Job job) throws BindingException {
-    try {
-      String appStr = job.getApp();
-      String resolvedAppStr = URIHelper.getData(appStr);
-      Draft2JobApp app = BeanSerializer.deserialize(JSONHelper.transformToJSON(resolvedAppStr), Draft2JobApp.class);
-      return new Draft2JobProcessor().process(new Draft2Job(app, job.getInputs()));
-    } catch (IOException e) {
-      throw new BindingException(e);
-    }
+    String resolvedAppStr = documentResolver.resolve(job.getApp());
+    Draft2JobApp app = BeanSerializer.deserialize(JSONHelper.transformToJSON(resolvedAppStr), Draft2JobApp.class);
+    return new Draft2JobProcessor().process(new Draft2Job(app, job.getInputs()));
   }
 
   public boolean isSelfExecutable(Job job) throws BindingException {
