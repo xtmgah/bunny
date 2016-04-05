68cbca8dab5a7d46418091866dbdf6bd734da75c
Sinisa Ivkovic <Sinisa@Sinisas-MacBook-Pro.local>
2016-04-05 14:19:48 +0200

    add default values for file config

--- rabix-executor/src/main/java/org/rabix/executor/config/FileConfig.java
+++ rabix-executor/src/main/java/org/rabix/executor/config/FileConfig.java
@@ -9,11 +9,11 @@ public class FileConfig {
   public static final String CHECKSUM_ALGORITHM = "rabix.checksum_algorithm";
 
   public static boolean calculateFileChecksum(Configuration configuration) {
-    return configuration.getBoolean(CALCULATE_FILE_CHECKSUM);
+    return configuration.getBoolean(CALCULATE_FILE_CHECKSUM, true);
   }
 
   public static HashAlgorithm checksumAlgorithm(Configuration configuration) {
-    return HashAlgorithm.valueOf(configuration.getString(CHECKSUM_ALGORITHM));
+    return HashAlgorithm.valueOf(configuration.getString(CHECKSUM_ALGORITHM, "SHA1"));
   }
 
 }
