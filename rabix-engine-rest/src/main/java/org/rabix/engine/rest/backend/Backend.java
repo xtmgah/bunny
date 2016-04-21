166f669e405d3ced1e678b5f413d620146a0bab5
Janko Simonovic <simonovic86@gmail.com>
2016-04-21 23:28:49 +0200

    fix Engine rest API

--- rabix-engine-rest/src/main/java/org/rabix/engine/rest/backend/Backend.java
+++ rabix-engine-rest/src/main/java/org/rabix/engine/rest/backend/Backend.java
@@ -1,5 +1,19 @@
 package org.rabix.engine.rest.backend;
 
+import org.rabix.engine.rest.backend.impl.BackendLocal;
+import org.rabix.engine.rest.backend.impl.BackendMQ;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({ 
+    @Type(value = BackendMQ.class, name = "MQ"),
+    @Type(value = BackendLocal.class, name = "LOCAL") })
+@JsonInclude(Include.NON_NULL)
 public interface Backend {
 
   public static enum BackendType {
