8f04b259467ea9a8e1bafbe4524f6c71b2e80aa3
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2016-05-18 11:25:43 +0200

    fix running script and remove unused import

--- rabix-backend-local/src/main/java/org/rabix/backend/local/BackendCommandLine.java
+++ rabix-backend-local/src/main/java/org/rabix/backend/local/BackendCommandLine.java
@@ -2,7 +2,6 @@ package org.rabix.backend.local;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
