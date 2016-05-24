6aeb746e3c25a59a3429bfb82c5a30d4c70d6f90
Janko Simonovic <simonovic86@gmail.com>
2016-05-25 00:03:26 +0200

    fix draft2 document resolver

--- rabix-bindings/src/main/java/org/rabix/bindings/protocol/draft2/resolver/Draft2DocumentResolver.java
+++ rabix-bindings/src/main/java/org/rabix/bindings/protocol/draft2/resolver/Draft2DocumentResolver.java
@@ -24,11 +24,14 @@ import org.rabix.common.helper.JSONHelper;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import com.google.common.base.Preconditions;
 
 public class Draft2DocumentResolver {
 
   public static final String RESOLVER_REFERENCE_KEY = "import";
+  public static final String RESOLVER_REFERENCE_INCLUDE_KEY = "include";
+  
   public static final String RESOLVER_JSON_POINTER_KEY = "$job";
   
   public static final String DOCUMENT_FRAGMENT_SEPARATOR = "#";
@@ -73,7 +76,7 @@ public class Draft2DocumentResolver {
       root = fragmentsCachePerUrl.get(fragment);
     }
     
-    traverse(appUrl, root, file, null, root, true);
+    traverse(appUrl, root, file, null, root);
 
     for (Draft2DocumentResolverReplacement replacement : getReplacements(appUrl)) {
       if (replacement.getParentNode().isArray()) {
@@ -91,9 +94,20 @@ public class Draft2DocumentResolver {
     return cache.get(appUrl);
   }
   
-  private static JsonNode traverse(String appUrl, JsonNode root, File file, JsonNode parentNode, JsonNode currentNode, boolean addReplacement) throws BindingException {
+  private static JsonNode traverse(String appUrl, JsonNode root, File file, JsonNode parentNode, JsonNode currentNode) throws BindingException {
     Preconditions.checkNotNull(currentNode, "current node id is null");
 
+    boolean isInclude = currentNode.has(RESOLVER_REFERENCE_INCLUDE_KEY);
+    if (isInclude) {
+      String path = currentNode.get(RESOLVER_REFERENCE_INCLUDE_KEY).textValue();
+      String content = loadContents(file, path);
+
+      Draft2DocumentResolverReference reference = new Draft2DocumentResolverReference(false, new TextNode(content));
+      getReferenceCache(appUrl).put(path, reference);
+      getReplacements(appUrl).add(new Draft2DocumentResolverReplacement(parentNode, currentNode, path));
+      return null;  // TODO fix
+    }
+    
     boolean isReference = currentNode.has(RESOLVER_REFERENCE_KEY);
     boolean isJsonPointer = currentNode.has(RESOLVER_JSON_POINTER_KEY) && parentNode != null; // we skip the first level $job
 
@@ -125,17 +139,15 @@ public class Draft2DocumentResolver {
           referenceDocumentRoot = findDocumentRoot(root, file, referencePath, isJsonPointer);
           parentChild = findReferencedNode(referenceDocumentRoot, referencePath);
         }
-        reference.setResolvedNode(traverse(appUrl, root, file, parentChild.parent, parentChild.child, true));
+        reference.setResolvedNode(traverse(appUrl, root, file, parentChild.parent, parentChild.child));
         reference.setResolving(false);
         getReferenceCache(appUrl).put(referencePath, reference);
       }
-      if (addReplacement) {
-        getReplacements(appUrl).add(new Draft2DocumentResolverReplacement(parentNode, currentNode, referencePath));
-      }
+      getReplacements(appUrl).add(new Draft2DocumentResolverReplacement(parentNode, currentNode, referencePath));
       return reference.getResolvedNode();
     } else if (currentNode.isContainerNode()) {
       for (JsonNode subnode : currentNode) {
-        traverse(appUrl, root, file, currentNode, subnode, addReplacement);
+        traverse(appUrl, root, file, currentNode, subnode);
       }
     }
     return currentNode;
@@ -198,40 +210,41 @@ public class Draft2DocumentResolver {
       if (parts.length > 2) {
         throw new BindingException("Invalid reference " + reference);
       }
-      String externalResource = parts[0];
-      if (externalResource.startsWith("http")) {
+      String contents = loadContents(file, parts[0]);
+      return JSONHelper.readJsonNode(JSONHelper.transformToJSON(contents));
+    }
+  }
+  
+  private static String loadContents(File file, String path) throws BindingException {
+    if (path.startsWith("http")) {
+      try {
+        URL website = new URL(path);
+        URLConnection connection = website.openConnection();
+        BufferedReader in = null;
+
         try {
-          URL website = new URL(externalResource);
-          URLConnection connection = website.openConnection();
-          BufferedReader in = null;
+          in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-          try {
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            StringBuilder response = new StringBuilder();
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-              response.append(inputLine);
-            }
-
-            String linkContents = response.toString();
-            return JSONHelper.readJsonNode(linkContents);
-          } finally {
-            if (in != null) {
-              in.close();
-            }
+          StringBuilder response = new StringBuilder();
+          String inputLine;
+          while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
           }
-        } catch (Exception e) {
-          throw new BindingException("Couldn't fetch contents from " + externalResource);
+          return response.toString();
+        } finally {
+          if (in != null) {
+            in.close();
+          }
         }
-      } else {
-        try {
-          String filePath = new File(file.getParentFile(), externalResource).getCanonicalPath();
-          String fileContents = FileUtils.readFileToString(new File(filePath), DEFAULT_ENCODING);
-          return JSONHelper.readJsonNode(JSONHelper.transformToJSON(fileContents));
-        } catch (IOException e) {
-          throw new BindingException("Couldn't fetch contents from " + externalResource);
-        }
+      } catch (Exception e) {
+        throw new BindingException("Couldn't fetch contents from " + path);
+      }
+    } else {
+      try {
+        String filePath = new File(file.getParentFile(), path).getCanonicalPath();
+        return FileUtils.readFileToString(new File(filePath), DEFAULT_ENCODING);
+      } catch (IOException e) {
+        throw new BindingException("Couldn't fetch contents from " + path);
       }
     }
   }
