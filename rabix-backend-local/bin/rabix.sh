f0d399e12cda8c61709381db5c3adb9e7e65b119
janko <janko.simonovic@sbgenomics.com>
2016-06-21 12:45:25 +0200

    refactor logging files

--- rabix-backend-local/bin/rabix.sh
+++ rabix-backend-local/bin/rabix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-command="java -jar $(dirname $0)/lib/rabix-backend-local-0.0.1-SNAPSHOT.jar"
+command="java -Dlogback.configurationFile=$(dirname $0)/config/logback.xml 2>/dev/null -jar $(dirname $0)/lib/rabix-backend-local-0.0.1-SNAPSHOT.jar 2>/dev/null"
 for i in "$@"
 do
     command="$command $i"
