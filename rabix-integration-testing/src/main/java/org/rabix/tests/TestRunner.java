075a22b8a858ecd6a8b0a74233d351d25b9aaa36
markosbg <marko.vukobrat@sbgenomics.com>
2016-08-10 20:09:15 +0200

    update TestRunner

--- rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
+++ rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
@@ -208,6 +208,8 @@ public class TestRunner {
     if (userDir == null) {
       throw new RabixTestException("null value for user.dir property");
     }
+    logger.info("Checkpoint2:\n");
+    logger.info(userDir + "/rabix-integration-tests/config/test");
     File configDir = new File(userDir + "/rabix-integration-tests/config/test");
     try {
       Iterator<File> iterator = FileUtils.iterateFiles(configDir, new String[] { "properties" }, true);
