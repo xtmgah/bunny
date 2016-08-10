9a178d109fa3bf1510e1435b80d2c46d41818124
markosbg <marko.vukobrat@sbgenomics.com>
2016-08-10 19:44:47 +0200

    update TestRunner

--- rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
+++ rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
@@ -56,7 +56,7 @@ public class TestRunner {
     logger.info("Extracting jar file");
     executeCommand("sudo tar -zxvf " + System.getProperty("user.dir")
         + "/rabix-backend-local/target/rabix-backend-local-0.0.1-SNAPSHOT-id3.tar.gz");
-    executeCommand("cp -a " + System.getProperty("user.dir") + "/rabix-tests/testbacklog .");
+    executeCommand("cp -a " + System.getProperty("user.dir") + "/rabix-integration-tests/testbacklog .");
 
     for (File child : directoryListing) {
       if (!child.toString().endsWith(".test.yaml"))
@@ -208,7 +208,7 @@ public class TestRunner {
     if (userDir == null) {
       throw new RabixTestException("null value for user.dir property");
     }
-    File configDir = new File(userDir + "/rabix-tests/config/test/core.properties");
+    File configDir = new File(userDir + "/rabix-integration-tests/config/test/core.properties");
     try {
       Iterator<File> iterator = FileUtils.iterateFiles(configDir, new String[] { "properties" }, true);
       while (iterator.hasNext()) {
